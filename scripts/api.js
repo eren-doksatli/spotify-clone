d08d837f98048d27c6b1411a8e53ae700eae76c4
eren-dox <eren.doksatli@gmail.com>
2024-03-31 14:22:24 +0300

    first commit

---
+++ scripts/api.js
@@ -0,0 +1,33 @@
+import { url, options } from "./constants.js";
+import { renderCards } from "./ui.js";
+
+export class API {
+  constructor() {
+    this.songs = [];
+  }
+
+  async getPopular() {
+    try {
+      const res = await fetch(url, options);
+      const data = await res.json();
+      this.songs = data.tracks;
+    } catch (err) {
+      console.log("Popüler verleri alırken hata oluştu..", err);
+    }
+  }
+
+  async searchMusic(query) {
+    const res = await fetch(
+      `https://shazam.p.rapidapi.com/search?term=${query}&locale=TR&offset=0&limit=20`,
+      options
+    );
+
+    const data = await res.json();
+
+    const newData = data.tracks.hits.map((song) => ({
+      ...song.track,
+    }));
+
+    renderCards(newData);
+  }
+}
---
+++ scripts/constants.js
@@ -0,0 +1,10 @@
+export const url =
+  "https://shazam.p.rapidapi.com/charts/track?locale=en-US&pageSize=20&startFrom=0";
+
+export const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "e82d60d14emshac33eedaf2a1be0p18ddb9jsn6b6da6aff6bd",
+    "X-RapidAPI-Host": "shazam.p.rapidapi.com",
+  },
+};
